25da68ae18881b38f600f71a76fcd3f662a7e56d
NimajnebEC <46959407+nimajnebec@users.noreply.github.com>
2024-07-05 13:09:27 +0100

    feat: start work on dynamic input system

--- src/lib/components/SightingInput.svelte
+++ src/lib/components/SightingInput.svelte
@@ -2,55 +2,54 @@
 	import { local } from "$lib/pouchdb";
 	import { onMount } from "svelte";
 
-	let classification: HTMLInputElement;
 	let location: HTMLInputElement;
-	let form: HTMLFormElement;
 
-	onMount(() => (location.value = window.localStorage.getItem("location") ?? ""));
+	let inputIds: string[] = [];
+	let inputBinds: { [key: string]: HTMLInputElement } = {};
 
-	async function submit() {
-		const data = Object.fromEntries(new FormData(form).entries());
-
-		const sighting = {
-			_id: new Date().getTime().toString(),
-			type: "sighting",
-
-			classification: data.classification.toString(),
-			location: data.location.toString(),
-		};
-
-		await local.put(sighting);
-
-		classification.value = "";
+	function updateInputs() {
+		inputIds = inputIds.filter((id) => inputBinds[id]?.value != "");
+		console.log(inputIds, inputBinds);
 	}
 
 	const saveLocation = () => window.localStorage.setItem("location", location.value);
+	onMount(() => (location.value = window.localStorage.getItem("location") ?? ""));
+	updateInputs();
 </script>
 
-<form on:submit|preventDefault={submit} bind:this={form}>
-	<input
-		type="text"
-		required
-		name="location"
-		autocomplete="off"
-		placeholder="Brighton"
-		on:focusout={saveLocation}
-		on:focusin={location.select}
-		bind:this={location}
-	/>
-	<input
-		type="text"
-		required
-		autocomplete="off"
-		placeholder="700128"
-		name="classification"
-		bind:this={classification}
-	/>
+<div class="container">
+	<label>
+		Location:
+		<input
+			type="text"
+			autocomplete="off"
+			placeholder="Brighton"
+			on:focusout={saveLocation}
+			on:focusin={location.select}
+			bind:this={location}
+		/>
+	</label>
+	<!-- svelte-ignore a11y-label-has-associated-control -->
+	<label>
+		Sightings:
+		<div class="container">
+			{#each Object.keys(inputIds) as id (id)}
+				<input
+					type="number"
+					autocomplete="off"
+					placeholder="700128"
+					name="classification"
+					on:change={updateInputs}
+					bind:this={inputBinds[id]}
+				/>
+			{/each}
+		</div>
+	</label>
 	<button type="submit">Submit</button>
-</form>
+</div>
 
 <style lang="scss">
-	form {
+	.container {
 		flex-direction: column;
 		display: flex;
 		gap: 10px;
